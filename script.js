d1e9e21855fcba064c534cc468e013d5d52e725a
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2020-07-17 12:17:20 -0500

    reach up until w^2*2

--- script.js
+++ script.js
@@ -1,42 +1,84 @@
-var ordinal = "0";
-var factor = "10";
-var template = `f<sub>${ordinal}</sub>(${factor})`;
-function ordinalUp() {
-  let filterOrd = ((/ω\d+/g.test(ordinal) == true && /ω\d+\+/g != true) ? "" : ordinal.match(/\d+$/g));
-  let filthOrd = ordinal.match(/ω\+|ω\d+\+|ω|ω\d+/g);
-  console.log(filthOrd);
-  console.log(ordinal);
-  let completeOrd = filthOrd + filterOrd;
-  let tempOrd = Number(filterOrd);
-  if (filthOrd == null) {
-    tempOrd++;
-    ordinal = tempOrd.toString();
-  } else if (ordinal == filthOrd) {
-    ordinal = `${filthOrd}+1`;
-    console.log("ordinal == filthOrd");
-  } else {
-    tempOrd++;
-    filterOrd = tempOrd.toString();
-    completeOrd = filthOrd + filterOrd;
-    ordinal = completeOrd;
+var game = {
+  ordinal: {
+    stringified: "0",
+    omegas: {
+      omegaCoeffs: [0],
+      omegaExps: [1]
+    },
+    numbers: 0
+  },
+  unlocks: {
+    prestige: false
   }
-  update();
+};
+var template = `f<sub>${game.ordinal.stringified}</sub>(${factor})`;
+var factor = 10;
+setInterval(update, 50);
+function increaseOrdinal() {
+  game.ordinal.numbers++;
+  stringifyOrdinal();
 }
-function ordinalMax() {
-  let beforeMatch = ordinal.match(/ω\d+/g);
-  let numMatch = ((beforeMatch != null) ? beforeMatch.match(/d+/g) : "");
-  if (Number(ordinal) >= factor) {
-    ordinal = "ω";
-  } else if (/ω+\d{2,}/g) {
-    ordinal = "ω2";
-  } else if (/ω\d+\+\d{2,}/g.test(ordinal) == true) {
-    ordinal = `ω${numMatch}`;
+function maxOrdinal() {
+  if (game.ordinal.numbers >= factor) {
+    game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1]++;
+    game.ordinal.numbers = 0;
   }
-  update();
+  if (game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1] >= factor) {
+    game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1] = 0;
+    game.ordinal.omegas.omegaCoeffs.unshift(1);
+    game.ordinal.omegas.omegaExps[game.ordinal.omegas.omegaExps.length - 1] = 2;
+  }
+  stringifyOrdinal();
+}
+function stringifyOrdinal() {
+  if (game.ordinal.omegas.omegaExps[game.ordinal.omegas.omegaExps.length - 1] > 1) {
+    if (game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 2] == 1) {
+      if (game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1] == 1) {
+        if (game.ordinal.numbers >= 1) {
+          game.ordinal.stringified = `ω<sup>${game.ordinal.omegas.omegaExps[game.ordinal.omegas.omegaExps.length - 1]}</sup>+ω+${game.ordinal.numbers}`;
+        } else {
+          game.ordinal.stringified = `ω<sup>${game.ordinal.omegas.omegaExps[game.ordinal.omegas.omegaExps.length - 1]}</sup>+ω`;
+        }
+      } else if (game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1] > 1) {
+        if (game.ordinal.numbers >= 1) {
+          game.ordinal.stringified = `ω<sup>${game.ordinal.omegas.omegaExps[game.ordinal.omegas.omegaExps.length - 1]}</sup>+ω${game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1]}+${game.ordinal.numbers}`;
+        } else {
+          game.ordinal.stringified = `ω<sup>${game.ordinal.omegas.omegaExps[game.ordinal.omegas.omegaExps.length - 1]}</sup>+ω${game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1]}`;
+        }
+      } else {
+        if (game.ordinal.numbers >= 1) {
+          game.ordinal.stringified = `ω<sup>${game.ordinal.omegas.omegaExps[game.ordinal.omegas.omegaExps.length - 1]}</sup>+${game.ordinal.numbers}`;
+        } else {
+          game.ordinal.stringified = `ω<sup>${game.ordinal.omegas.omegaExps[game.ordinal.omegas.omegaExps.length - 1]}</sup>`;
+        }
+      }
+    }
+  } else if (game.ordinal.omegas.omegaExps[game.ordinal.omegas.omegaExps.length - 1] == 1) {
+    if (game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1] > 1) {
+      if (game.ordinal.numbers > 0) {
+        game.ordinal.stringified = `ω${game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1]}+${game.ordinal.numbers}`;
+      } else {
+        game.ordinal.stringified = `ω${game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1]}`;
+      }
+    } else if (game.ordinal.omegas.omegaCoeffs[game.ordinal.omegas.omegaCoeffs.length - 1] == 1) {
+      if (game.ordinal.numbers > 0) {
+        game.ordinal.stringified = `ω+${game.ordinal.numbers}`;
+      } else {
+        game.ordinal.stringified = `ω`;
+      }
+    } else {
+      game.ordinal.stringified = `${game.ordinal.numbers}`;
+    }
+  } /* else {
+    other stuff
+  } */
 }
 function update() {
-  template = `f<sub>${ordinal}</sub>(${factor})`;
+  template = `f<sub>${game.ordinal.stringified}</sub>(${factor})`;
   get("fgh").innerHTML = template;
+  if (!game.unlocks.prestige && game.ordinal.omegas >= 1) {
+    game.unlocks.prestige = true;
+  }
 }
 function get(id) {
   return document.getElementById(id);
